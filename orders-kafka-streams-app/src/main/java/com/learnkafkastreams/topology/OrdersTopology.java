a2f5947a3547e4e746c926e2065bc311f1cac4a0
sagarmakote <sagar.makote@gmail.com>
2024-11-20 17:38:48 +0530

    Delete orders-kafka-streams-app directory

--- orders-kafka-streams-app/src/main/java/com/learnkafkastreams/topology/OrdersTopology.java
+++
@@ -1,9 +0,0 @@
-package com.learnkafkastreams.topology;
-
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-public class OrdersTopology {
-    public static final String ORDERS = "orders";
-    public static final String STORES = "stores";
-}
